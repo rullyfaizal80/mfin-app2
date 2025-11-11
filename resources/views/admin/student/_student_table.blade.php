174312edde1a4f7152f73e322679743ebce31a24
Rully Faizal <rullyfaizal@mimha.sch.id>
2025-11-11 14:59:37 +0700

    hapus siswa

--- resources/views/admin/student/_student_table.blade.php
+++ resources/views/admin/student/_student_table.blade.php
@@ -33,7 +33,7 @@
                         <a href="#" class="btn btn-sm btn-outline-warning" title="Edit">
                             <i class="bi bi-pencil-square"></i>
                         </a>
-                        <form action="#" method="POST" class="d-inline" onsubmit="return confirm('Apakah Anda yakin ingin menghapus data ini?');">
+                        <form action="{{ route('student.destroy', $student->id) }}" method="POST" class="d-inline" onsubmit="return confirm('Apakah Anda yakin ingin menghapus data ini?');">
                             @csrf
                             @method('DELETE')
                             <button type="submit" class="btn btn-sm btn-outline-danger" title="Hapus">
