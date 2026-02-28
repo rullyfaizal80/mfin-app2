3a171c53747b7b249c0bf97d5b4ca9b761b6bd7a
Rully Faizal <rullyfaizal@mimha.sch.id>
2026-02-28 13:08:00 +0700

    280220261307

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ use App\Http\Controllers\Admin\SchoolReportController;
 use App\Http\Controllers\Admin\ReportStudentController;
 use App\Http\Controllers\Admin\ReportTeacherController;
 use App\Http\Controllers\Fincom\PaymentController;
+use App\Http\Controllers\Fincom\TransexpenseController;
 
 /*
  * |--------------------------------------------------------------------------
@@ -210,5 +211,10 @@ Route::middleware(['custom.auth'])->group(function () {
         // Route untuk AJAX pencarian siswa
         Route::get('/ajax-student', [PaymentController::class, 'ajaxStudent'])->name('ajax_student');
     });
+
+    Route::prefix('fincom/transexpense')->name('fincom.transexpense.')->group(function () {
+    Route::get('/', [TransexpenseController::class, 'index'])->name('index');
+    // Nanti kita akan tambahkan route create, edit, dll di sini
+});
     
 });
